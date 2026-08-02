0cf3db39829ec5655b8e69b21c24cb484d5af9ea
Willhelm-Alto <souzaguilherme1234567890@gmail.com>
2026-08-02 01:41:14 -0300

    bottom sheet & list view

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gym Tracker',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepOrange)),
       home: const Home(),
     );
   }
@@ -26,6 +24,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool showBottomSheet = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,15 +38,33 @@ class _HomeState extends State<Home> {
       body: ListView.builder(
         itemCount: 4,
         itemBuilder: (context, i) {
-          return ExerciseTile();
+          return ExerciseTile(onTap: () => setState(() => showBottomSheet = !showBottomSheet));
         },
       ),
+      bottomSheet: showBottomSheet ? 
+        BottomSheet(
+          constraints: BoxConstraints(maxHeight: 100),
+          backgroundColor: Colors.amber,
+          onClosing: (){}, builder: (context) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: (){}, 
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(), 
+                  padding: const EdgeInsets.all(24)),
+                child: Icon(Icons.check))
+            ],
+            );
+      }) : null,
     );
   }
 }
 
 class ExerciseTile extends StatefulWidget {
-  ExerciseTile({super.key});
+  const ExerciseTile({required this.onTap, super.key});
+  final Function onTap;
 
   @override
   ExerciseTileState createState() => ExerciseTileState();
@@ -58,8 +76,15 @@ class ExerciseTileState extends State<ExerciseTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(""),
-      // trailing: Switch(value: done, onChanged: (value) => setState((){done = value;})),
+      title: Text("SUPINO RETO"),
+      subtitle: Text("4Kg \t 3/12", style: TextStyle(color: Colors.grey)),
+      trailing: Switch(
+        value: done,
+        onChanged: (value) => setState(() {
+          done = value;
+        }),
+      ),
+      onTap: ()=>widget.onTap(),
     );
   }
 }
